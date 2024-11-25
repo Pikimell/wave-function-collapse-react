3040ba08f4c55299fe82b2650e957baac308a631
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-26 01:58:32 +0200

    frame list style

--- src/components/FrameList/FrameList.jsx
+++ src/components/FrameList/FrameList.jsx
@@ -2,7 +2,13 @@ import style from './FrameList.module.css';
 import { useState } from 'react';
 
 const FrameList = ({}) => {
-  return <div className={style.container}>FrameList</div>;
+  return (
+    <div className={style.container}>
+      <h4 className={style.title}>FRAMES</h4>
+
+      <ul className={style.list}></ul>
+    </div>
+  );
 };
 
 export default FrameList;
--- src/components/FrameRules/FrameRules.jsx
+++ src/components/FrameRules/FrameRules.jsx
@@ -2,7 +2,11 @@ import style from './FrameRules.module.css';
 import { useState } from 'react';
 
 const FrameRules = ({}) => {
-  return <div className={style.container}>FrameRules</div>;
+  return (
+    <div className={style.container}>
+      <h4 className={style.title}>RULES</h4>
+    </div>
+  );
 };
 
 export default FrameRules;
